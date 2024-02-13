785707e17f33c1454b04fe8fd61b6b2a199a2727
Michael <gerentedev@outlook.com>
2024-02-13 08:17:13 -0300

    Exercicio do radius

---
+++ src/util/Calculator.java
@@ -0,0 +1,15 @@
+package util;
+
+public class Calculator {
+public final double PI = 3.1415159;
+private double volume(double radius) {
+	// TODO Auto-generated method stub
+	return 2.0*PI*radius;
+}
+private double circumference(double radius) {
+	// TODO Auto-generated method stub
+	
+	return 4.0*PI*radius*radius*radius/3.0;
+}
+
+}
